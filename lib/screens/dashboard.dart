9d0f57a905e0f2736a4cfa1bea720fa1a22e2676
Carolina Bonturi <carolina.bonturi@nubank.com.br>
2020-10-19 16:04:12 -0300

    Add new transaction feature in dashboard

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -16,45 +16,63 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('images/bytebank_logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => ContactsList(),
-                    ),
-                  );
-                },
-                child: Container(
-                  padding: EdgeInsets.all(8.0),
-                  height: 100,
-                  width: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(
-                        Icons.people,
-                        color: Colors.white,
-                        size: 24.0,
-                      ),
-                      Text(
-                        'Contacts',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          )
+          Row(
+            children: [
+              _FeatureItem(Icons.monetization_on, 'Transfer'),
+              _FeatureItem(Icons.description, 'Transaction Feed')
+            ],
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+
+  final IconData _icon;
+  final String _featureName;
+
+  _FeatureItem(this._icon, this._featureName);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => ContactsList(),
+              ),
+            );
+          },
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            height: 100,
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  _icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  _featureName,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
